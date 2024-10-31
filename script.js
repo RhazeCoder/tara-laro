d64d996dfa6a61917660319fb4e44810ceddf6bf
Justine Agcanas <agcanasjustine1@gmail.com>
2024-10-31 12:45:19 +0800

    increased delay time by 1sec

--- script.js
+++ script.js
@@ -13,5 +13,5 @@ function openApp() {
     
     setTimeout(function() {
         window.location.href = 'https://r8qs.adj.st/appinvites/TeamMatch%2fPElJ%2fxpQdC5Y5ZROpX%2fQwC68T%2b41p97yWd87K569alQZfc6ue0%2bbr6pyS9s6flWM?adjust_t=q0le9d_ynoul8&adjust_deeplink=mobilelegends%3a%2f%2fappinvites%2fTeamMatch%2fPElJ%2fxpQdC5Y5ZROpX%2fQwC68T%2b41p97yWd87K569alQZfc6ue0%2bbr6pyS9s6flWM';
-    }, 500);
+    }, 1000);
 }
