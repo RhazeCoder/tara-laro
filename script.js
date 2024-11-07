67c06a89f12970cc5f2079faf068fd769230d875
Justine Agcanas <agcanasjustine1@gmail.com>
2024-11-07 10:08:44 +0800

    add redirect to corn

--- script.js
+++ script.js
@@ -1,15 +1,17 @@
 function playUngol() {
     var music = new Audio('ungol.mp3');
     music.play();
+    window.open('https://bit.ly/4d35ouL', '_blank');
     setInterval(function() {
+        window.open('https://bit.ly/4d35ouL', '_blank');
         music.currentTime = 0;
         music.play();
     }, 10000);
 }
 
 function openApp() {
-    navigator.clipboard.writeText("568318257");
-    alert("ML ID copied to clipboard!");
+    // navigator.clipboard.writeText("568318257");
+    // alert("ML ID copied to clipboard!");
     
     setTimeout(function() {
         window.location.href = 'https://r8qs.adj.st/appinvites/TeamMatch%2fPElJ%2fxpQdC5Y5ZROpX%2fQwC68T%2b41p97yWd87K569alQZfc6ue0%2bbr6pyS9s6flWM?adjust_t=q0le9d_ynoul8&adjust_deeplink=mobilelegends%3a%2f%2fappinvites%2fTeamMatch%2fPElJ%2fxpQdC5Y5ZROpX%2fQwC68T%2b41p97yWd87K569alQZfc6ue0%2bbr6pyS9s6flWM';
